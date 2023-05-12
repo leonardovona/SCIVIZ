5ab15096943ef1e531a3667e79743e5d807cb191
leonardovona <leonardo.vona@gmail.com>
2023-05-12 17:39:05 +0200

    Updated stacked bar chart

---
+++ map.js
@@ -0,0 +1,130 @@
+const value = document.querySelector("#year")
+const input = document.querySelector("#year_input")
+value.textContent = input.value
+input.addEventListener("input", (event) => {
+    value.textContent = event.target.value
+    handle_update(event.target.value)
+})
+
+const width_map = 600, height_map = 550
+
+var svg_map = d3.select("#map")
+    .classed("svg-container", true)
+    .append("svg")
+    .attr(
+        "viewBox",
+        `0 0 ${width_map} ${height_map}`
+    )
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .classed("svg-content-responsive", true)
+
+var colorScale = d3.scaleThreshold()
+    .domain([0.01, 1, 2, 4, 8, 16, 32])
+    .range(d3.schemeBlues[7]);
+
+const data = new Map()
+// Create a function that takes a dataset as input and update the plot:
+
+// Map and projection
+var path = d3.geoPath();
+var projection = d3.geoMercator()
+    .scale(420)
+    .center([13, 55])
+    .translate([width_map / 2, height_map / 2]);
+
+// Data and color scale
+
+
+var tooltip = d3.select("#map")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+
+// Load external data and boot
+Promise.all([
+    d3.json("./resources/europe.geojson"),
+    d3.csv("./resources/wei.csv")]).then(function (loadData) {
+        let topo = loadData[0]
+
+        let wei = loadData[1]
+
+        wei.forEach(function (d) {
+            data.set(d.ID, d)
+        })
+
+        let mouseOver = function (d) {
+            if (EU_members.includes(d.srcElement.__data__.properties.ID)) {
+                d3.selectAll(".Country")
+                    .transition()
+                    .duration(200)
+                    .style("opacity", .5)
+                    .style("stroke", "transparent")
+                d3.select(this)
+                    .transition()
+                    .duration(200)
+                    .style("opacity", 1)
+                    .style("stroke", "black")
+                tooltip
+                    .transition()
+                    .duration(300)
+                    .style("opacity", 1)
+            }
+        }
+
+        let mouseMove = function (d) {
+            if (EU_members.includes(d.srcElement.__data__.properties.ID)) {
+                tooltip
+                    .html(d.srcElement.__data__.properties.NAME + ": " + d.srcElement.__data__.total)
+                    .style("left", (d.clientX + 20) + "px")
+                    .style("top", (d.clientY) + "px")
+            }
+        }
+
+        let mouseLeave = function (d) {
+            if (EU_members.includes(d.srcElement.__data__.properties.ID)) {
+                d3.selectAll(".Country")
+                    .transition()
+                    .duration(200)
+                    .style("opacity", 1)
+                    .style("stroke", "transparent")
+                tooltip.transition().duration(300)
+                    .style("opacity", 0)
+            }
+        }
+
+        // Draw the map
+        svg_map.append("g")
+            .selectAll("path")
+            .data(topo.features)
+            .enter()
+            .append("path")
+            // draw each country
+            .attr("d", d3.geoPath()
+                .projection(projection)
+            )
+            .style("stroke", "transparent")
+            .attr("class", function (d) { return "Country" })
+            .style("opacity", 1)
+            .on("mouseover", mouseOver)
+            .on("mousemove", mouseMove)
+            .on("mouseleave", mouseLeave)
+
+        handle_update("2019")
+
+
+    })
+
+function handle_update(year) {
+    d3.selectAll(".Country")
+        .transition()
+        .duration(1000)
+        .attr("fill", function (d) {
+            country = data.get(d.properties.ID)
+            if (country !== undefined) {
+                d.total = country[year]
+            } else {
+                d.total = 0
+            }
+            return colorScale(d.total);
+        })
+}
---
+++ stacked_bar_chart.js
@@ -0,0 +1,136 @@
+//Stacked bar chart
+
+// set the dimensions and margins of the graph
+const margin_sb = { top: 10, right: 30, bottom: 20, left: 70 },
+    width_sb = 600 - margin_sb.left - margin_sb.right,
+    height_sb = 400 - margin_sb.top - margin_sb.bottom;
+
+// append the svg object to the body of the page
+const svg_sb = d3
+    .select("#stackedBarChart")
+    .classed("svg-container", true)
+    .append("svg")
+    .attr("class", "chart")
+    .attr(
+        "viewBox",
+        `0 0 ${width_sb + margin_sb.left + margin_sb.right} ${height_sb + margin_sb.top + margin_sb.bottom}`
+    )
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .classed("svg-content-responsive", true)
+    .append("g")
+
+// Add X axis
+x_axis = svg_sb.append("g")
+    .attr("transform", "translate(0," + height_sb + ")")
+
+y = d3.scaleLinear()
+    .domain([0, 170000000])
+    .range([height_sb, 0]);
+
+// Not sure if goes here
+svg_sb.append("g")
+    .call(d3.axisLeft(y));
+
+var bars = svg_sb.append("g").attr("class", "bars");
+
+var groups;
+var pollutants_data;
+d3.csv("./resources/2007_water_pollutants.csv").then(function (data) {
+    pollutants_data = data;
+
+    // List of subgroups = header of the csv files = soil condition here
+    subgroups = pollutants_data.columns.slice(1)
+
+    // color palette = one color per subgroup
+    color = d3.scaleOrdinal()
+        .domain(subgroups)
+        .range(['#e41a1c', '#377eb8', '#4daf4a', '#834aaf'])
+
+    // console.log(stackedData)
+    update_sb()
+})
+
+function update_sb(pollutant = "all") {
+    pollutants_data.sort(function (b, a) {
+        if (pollutant == "all") {
+            a_nitrogen = parseFloat(a.Nitrogen) || 0;
+            a_phosphorus = parseFloat(a.Phosphorus) || 0;
+            a_toc = parseFloat(a.TOC) || 0;
+            a_heavymetals = parseFloat(a['Heavy metals (Cd, Hg, Ni, Pb)']) || 0;
+
+            b_nitrogen = parseFloat(b.Nitrogen) || 0;
+            b_phosphorus = parseFloat(b.Phosphorus) || 0;
+            b_toc = parseFloat(b.TOC) || 0;
+            b_heavymetals = parseFloat(b['Heavy metals (Cd, Hg, Ni, Pb)']) || 0;
+
+            return (a_nitrogen + a_phosphorus + a_toc + a_heavymetals) - (b_nitrogen + b_phosphorus + b_toc + b_heavymetals)
+        } else {
+            return parseFloat(a[pollutant]) - parseFloat(b[pollutant])
+        }
+    })
+
+    groups = pollutants_data.map(d => (d.countryName));
+
+    x = d3.scaleBand()
+        .domain(groups)
+        .range([0, width_sb])
+        .padding([0.2])
+
+    x_axis
+        .transition().duration(1000)
+        .call(d3.axisBottom(x).tickSizeOuter(0));
+
+    //stack the data? --> stack per subgroup
+    const stackedData = d3.stack()
+        .keys(subgroups)
+        .order(d3.stackOrderDescending)
+        (pollutants_data)
+
+
+    bars
+        .selectAll("g")
+        .data(stackedData)
+        .join(
+            enter => enter
+                .append("g")
+                .attr("fill", d => color(d.key)),
+            null,
+            exit => {
+                exit
+                    .transition()
+                    .duration(500)
+                    .style("fill-opacity", 0)
+                    .remove();
+            }
+        ).selectAll("rect")
+        .data(d => d, d => d.data.countryName)
+        .join(
+            enter => enter
+                .append("rect")
+                .attr("class", "bar")
+                .attr("x", d => {
+                    return x(d.data.countryName);
+                })
+                .attr("y", d => y(d[1]))
+                .attr("width", x.bandwidth())
+                .attr("heigth", d => y(d[0]) - y(d[1])),
+            null,
+            exit => {
+                exit
+                    .transition()
+                    .duration(500)
+                    .style("fill-opacity", 0)
+                    .remove();
+            }
+        )
+        .transition(d3.transition().duration(1000))
+        .delay((d, i) => i * 20)
+        .attr("x", d => x(d.data.countryName))
+        .attr("y", d => {
+            return y(d[1]);
+        })
+        .attr("width", x.bandwidth())
+        .attr("height", d => {
+            return y(d[0]) - y(d[1]);
+        });
+}
